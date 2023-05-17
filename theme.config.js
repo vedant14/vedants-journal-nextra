804ed9c220323fb56aa6f527cbec63ecdaca54d0
Vedant Lohbare <31346595+vedant14@users.noreply.github.com>
2023-05-17 17:21:41 +0530

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -1,38 +1,10 @@
-const YEAR = new Date().getFullYear()
-
-export default {
-  readMore: 'Expand →',
-  titleSuffix: ' | Vedant Lohbare',
-  postFooter: (
-    <>
-      <hr />
-      <p>
-        This site has no analytics. For any feedback / comments / suggestions or
-        to just have a cup of coffee, you can{' '}
-        <a href="https://twitter.com/VedantLohbare/" target="_blank">
-          DM me here
-        </a>
-        .{' '}
-      </p>
-    </>
-  ),
-  footer: (
-    <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Vedant Lohbare.
-      <a href="https://twitter.com/VedantLohbare/" target="_blank">
-        Twitter
-      </a>
-      <style jsx>{`
-        a {
-          float: right;
-        }
-        @media screen and (max-width: 480px) {
-          article {
-            padding-top: 2rem;
-            padding-bottom: 4rem;
-          }
-        }
-      `}</style>
-    </small>
+import type { NextraThemeLayoutProps } from 'nextra'
+ 
+export default function Layout({ children }: NextraThemeLayoutProps) {
+  return (
+    <div>
+      <h1>My Theme</h1>
+      <div style={{ border: '1px solid' }}>{children}</div>
+    </div>
   )
 }
